51560cd33fc44f58d7ee16e69bb5176d4e3a90ea
Nitendra Yadav <yadav.yadav92@gmail.com>
2024-08-31 10:59:44 +0530

    small spell correction

--- index.py
+++ index.py
@@ -1,3 +1,5 @@
+#
+#
 a = 5
 b = 5
 c = 4
@@ -6,4 +8,4 @@ s = (a + b + c) / 2
 
 area = (s*(s-a)*(s-b)*(s-c)) ** 0.5
 
-pront("The area of the trangle is %0.2f"%area)
+print("The area of the trangle is %0.2f"%area)
